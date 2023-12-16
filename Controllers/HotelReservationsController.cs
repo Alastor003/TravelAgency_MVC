6bf4e611fd06b76d870302db3c678eb76b074da9
RodriCz <rodrigocruz_96@hotmail.com>
2023-12-16 18:10:56 -0300

    Add vista funcional.

--- Controllers/HotelReservationsController.cs
+++ Controllers/HotelReservationsController.cs
@@ -174,5 +174,9 @@ namespace TravelAgency_MVC.Controllers
         {
           return (_context.hotelReservations?.Any(e => e.ID == id)).GetValueOrDefault();
         }
+        public async Task<IActionResult> EditHotel()
+        {
+            return View();
+        }
     }
 }
